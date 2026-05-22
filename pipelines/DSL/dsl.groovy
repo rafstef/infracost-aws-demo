e526279f8eb22df0d5b9b0b4bad68c16fa073e89
Stefanile Raffaele <raffaele.stefanile@MacBook-Pro-di-Support.local>
2026-05-22 17:49:05 +0200

    test folder

--- pipelines/DSL/dsl.groovy
+++ pipelines/DSL/dsl.groovy
@@ -1,4 +1,54 @@
-pipelineJob('AWS/infracost-aws-demo') {
+pipelineJob('AWS/PROD/infra-1') {
+    description('Terraform + Infracost pipeline for infracost-aws-demo')
+
+    definition {
+        cpsScm {
+            scm {
+                git {
+                    remote {
+                        url('https://github.com/rafstef/infracost-aws-demo.git')
+                    }
+                    branch('*/master')
+                    extensions {
+                        cleanBeforeCheckout()
+                    }
+                }
+            }
+            scriptPath('Jenkinsfile')
+            lightweight(true)
+        }
+    }
+
+    properties {
+        disableConcurrentBuilds()
+    }
+}
+pipelineJob('AWS/STAGE/infra-1') {
+    description('Terraform + Infracost pipeline for infracost-aws-demo')
+
+    definition {
+        cpsScm {
+            scm {
+                git {
+                    remote {
+                        url('https://github.com/rafstef/infracost-aws-demo.git')
+                    }
+                    branch('*/stage')
+                    extensions {
+                        cleanBeforeCheckout()
+                    }
+                }
+            }
+            scriptPath('Jenkinsfile')
+            lightweight(true)
+        }
+    }
+
+    properties {
+        disableConcurrentBuilds()
+    }
+}
+pipelineJob('AWS/DEV/infra-1') {
     description('Terraform + Infracost pipeline for infracost-aws-demo')
 
     definition {
