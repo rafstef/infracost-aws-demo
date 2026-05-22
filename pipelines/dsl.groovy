25eb9366c04c0100de43755126e2dcac3343c507
Stefanile Raffaele <raffaele.stefanile@MacBook-Pro-di-Support.local>
2026-05-22 16:55:47 +0200

    test

---
+++ pipelines/dsl.groovy
@@ -0,0 +1,25 @@
+pipelineJob('infracost-aws-demo') {
+    description('Terraform + Infracost pipeline for infracost-aws-demo')
+
+    definition {
+        cpsScm {
+            scm {
+                git {
+                    remote {
+                        url('https://github.com/rafstef/infracost-aws-demo.git')
+                    }
+                    branch('*/master')
+                    extensions {
+                        cleanBeforeCheckout()
+                    }
+                }
+            }
+            scriptPath('Jenkinsfile')
+            lightweight(true)
+        }
+    }
+
+    properties {
+        disableConcurrentBuilds()
+    }
+}
