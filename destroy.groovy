e65bc7543c658ef8d50a95b39436e73104b668b7
Stefanile Raffaele <raffaele.stefanile@MacBook-Pro-di-Support.local>
2026-05-25 16:47:25 +0200

    etst

--- destroy.groovy
+++ destroy.groovy
@@ -44,7 +44,7 @@ pipeline {
         stage('TF Init and validations checks') {
             steps {
                 sh "terraform version"
-                sh "terraform init -backend-config=bucket=infratest-demo-${env.ENV_NAME}-tfstate -no-color"
+                sh "terraform init -backend-config=bucket=infracost-demo-${env.ENV_NAME} -no-color"
                 sh "terraform init -upgrade"
                 sh "terraform validate -no-color"
             }
@@ -52,7 +52,7 @@ pipeline {
         stage('TF Plan creation') {
             steps {
                 sh "terraform version"
-                sh "terraform init -backend-config=bucket=infratest-demo-${env.ENV_NAME}-tfstate -no-color"
+                sh "terraform init -backend-config=bucket=infracost-demo-${env.ENV_NAME} -no-color"
                 sh "terraform plan -destroy -no-color -input=false -var-file=envvars/${env.ENV_NAME}.tfvars -out terraform-destroy-plan-${env_name}.plan"
             }
         }
@@ -62,7 +62,7 @@ pipeline {
                     approve_plan=askUserInput("Apply Terraform destroy plan?","NO\nYES","NO",300)
                     if( approve_plan == "YES"){
                         sh "terraform version"
-                        sh "terraform init -backend-config=bucket=infratest-demo-${env.ENV_NAME}-tfstate -no-color"
+                        sh "terraform init -backend-config=bucket=infracost-demo-${env.ENV_NAME} -no-color"
                         sh "terraform apply -destroy -no-color -input=false terraform-destroy-plan-${env_name}.plan"
                     }else{
                         echo "TF destroy plan not approved. Skip Apply . . . "
